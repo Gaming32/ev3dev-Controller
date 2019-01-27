064a923067dc8c352139dfdea5cc82831694b2c5
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2019-01-27 14:42:00 -0600

    1/26/19

--- ev3dev Controller/Properties/AssemblyInfo.cs
+++ ev3dev Controller/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ev3dev Controller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Gaming32")]
 [assembly: AssemblyProduct("ev3dev Controller")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.*")]
